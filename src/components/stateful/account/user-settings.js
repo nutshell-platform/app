471715002a26942658041ec0d187062fbe19029f
Mentor Palokaj <mentor@palokaj.co>
2020-04-17 17:53:08 +0200

    Better scripts and add new user validation

--- src/components/stateful/account/user-settings.js
+++ src/components/stateful/account/user-settings.js
@@ -109,8 +109,6 @@ class UserSettings extends Component {
 		const { loading, user: newuser, settings: newsettings, passwordRequired } = this.state
 		const { settings, user } = this.props
 
-		console.log( this.state )
-
 		if( !user || loading ) return <Loading message={ loading } />
 
 		return <Container>
--- src/components/stateless/account/user-settings.js
+++ src/components/stateless/account/user-settings.js
@@ -3,35 +3,39 @@ import { View } from 'react-native'
 import { Card, Main, Title, Input, Button, Subheading, Divider, Toggle, HelperText, Text } from '../common/generic'
 import ImagePicker from '../../stateful/common/image-picker'
 
-export const Settings = ( { children, avatarSize=100, user={}, changeUser, settings, changeNotification, changeSetting, saveChanges, passwordRequired } ) => <Main.Center>
-	<View style={ { paddingVertical: avatarSize/2 } }>
-		<Card style={ { paddingTop: 0, width: 400 } } >
-			<ImagePicker image={ user.newavatar || user.avatar } size={ avatarSize } style={ { marginTop: -avatarSize/2, marginBottom: 20 } } onSelected={ image => changeUser( 'newavatar', image ) } />
-			<Title style={ { textAlign: 'center' } }>{ !user.settings.notifications && 'Welcome ' }{ user.name || user.email }{ user.settings.notifications ? '\'s settings' : '!' }</Title>
-			{ !user.settings.notifications && <Text style={ { marginTop: 10, textAlign: 'center' } }>Welcome to Nutshell { user.name }! Please let us know if these settings are ok with you, you can change them any time in the 'settings' section.</Text> }
-			<Divider />
-			
-			{ /* User data */ }
-			<Subheading>Account settings</Subheading>
-			<Input label='name' value={ user.name } info='This lets your friends find you.' onChangeText={ t => changeUser( 'name', t ) } />
-			<Input label='email' value={ user.email } info='Nobody can see this publicly.' onChangeText={ t => changeUser( 'email', t ) } />
-			<Input secureTextEntry label='new password' value={ user.newpassword || '' } onChangeText={ t => changeUser( 'newpassword', t ) } />
+export const Settings = ( { children, avatarSize=100, user={}, changeUser, settings={}, changeNotification, changeSetting, saveChanges, passwordRequired } ) => {
 
-			{ /* Notification prefs */ }
-			<Subheading style={ { marginVertical: 20, flexDirection: 'row' } }>Notification preferences</Subheading>
-			<Toggle onToggle={ f => changeNotification( 'writeReminder', !settings.notifications.writeReminder ) } value={ settings.notifications.writeReminder } style={ { marginTop: 10 } } label='Write your nutshell (weekly)' info="You get to write 1 nutshell message per week, this notification reminds you to use it before it expires" />
-			<Toggle onToggle={ f => changeNotification( 'readReminder', !settings.notifications.readReminder ) } value={ settings.notifications.readReminder } style={ { marginTop: 10 } } label='Unread nutshells (weekly)' info="A weekly reminder of how many nutshells you have not yet read" />
-			<Toggle onToggle={ f => changeNotification( 'newFollower', !settings.notifications.newFollower ) } value={ settings.notifications.newFollower } style={ { marginTop: 10 } } label='New followers (realtime)' info="Get a notification when a new friend starts following you" />
-			<Toggle onToggle={ f => changeNotification( 'friendJoined', !settings.notifications.friendJoined ) } value={ settings.notifications.friendJoined } style={ { marginTop: 10 } } label='Friend joined (realtime)' info="Get a notification when a friend joins nutshell" />
+	const newUser = !user?.settings?.notifications
 
-			{ /* Password required */ }
-			{ passwordRequired && <React.Fragment>
-				<Input secureTextEntry label='current password' info='Current password required for this change' error={ true } value={ user.currentpassword || '' } onChangeText={ t => changeUser( 'currentpassword', t ) } />
-			</React.Fragment> }
-			<Button onPress={ saveChanges }>Save changes</Button>
-		</Card>
-	</View>
+	return <Main.Center>
+		<View style={ { paddingVertical: avatarSize/2 } }>
+			<Card style={ { paddingTop: 0, width: 400 } } >
+				<ImagePicker image={ user.newavatar || user.avatar } size={ avatarSize } style={ { marginTop: -avatarSize/2, marginBottom: 20 } } onSelected={ image => changeUser( 'newavatar', image ) } />
+				<Title style={ { textAlign: 'center' } }>{ !user?.settings?.notifications && 'Welcome ' }{ user.name || user.email }{ user?.settings?.notifications ? '\'s settings' : '!' }</Title>
+				{ newUser && <Text style={ { marginTop: 10, textAlign: 'center' } }>Welcome to Nutshell { user.name }! Please let us know if these settings are ok with you, you can change them any time in the 'settings' section.</Text> }
+				<Divider />
+				
+				{ /* User data */ }
+				<Subheading>Account settings</Subheading>
+				<Input label='name' value={ user.name } info='This lets your friends find you.' onChangeText={ t => changeUser( 'name', t ) } />
+				<Input label='email' value={ user.email } info='Nobody can see this publicly.' onChangeText={ t => changeUser( 'email', t ) } />
+				<Input secureTextEntry label='new password' value={ user.newpassword || '' } onChangeText={ t => changeUser( 'newpassword', t ) } />
 
-</Main.Center>
+				{ /* Notification prefs */ }
+				<Subheading style={ { marginVertical: 20, flexDirection: 'row' } }>Notification preferences</Subheading>
+				<Toggle onToggle={ f => changeNotification( 'writeReminder', !settings?.notifications?.writeReminder ) } value={ settings.notifications.writeReminder } style={ { marginTop: 10 } } label='Write your nutshell (weekly)' info="You get to write 1 nutshell message per week, this notification reminds you to use it before it expires" />
+				<Toggle onToggle={ f => changeNotification( 'readReminder', !settings?.notifications?.readReminder ) } value={ settings.notifications.readReminder } style={ { marginTop: 10 } } label='Unread nutshells (weekly)' info="A weekly reminder of how many nutshells you have not yet read" />
+				<Toggle onToggle={ f => changeNotification( 'newFollower', !settings?.notifications?.newFollower ) } value={ settings.notifications.newFollower } style={ { marginTop: 10 } } label='New followers (realtime)' info="Get a notification when a new friend starts following you" />
+				<Toggle onToggle={ f => changeNotification( 'friendJoined', !settings?.notifications?.friendJoined ) } value={ settings.notifications.friendJoined } style={ { marginTop: 10 } } label='Friend joined (realtime)' info="Get a notification when a friend joins nutshell" />
+
+				{ /* Password required */ }
+				{ passwordRequired && <React.Fragment>
+					<Input secureTextEntry label='current password' info='Current password required for this change' error={ true } value={ user.currentpassword || '' } onChangeText={ t => changeUser( 'currentpassword', t ) } />
+				</React.Fragment> }
+				<Button onPress={ saveChanges }>{ newUser ? 'Confirm settings and continue' : 'Save changes' }</Button>
+			</Card>
+		</View>
+	</Main.Center>
+}
 
 export const another = true
