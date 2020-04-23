0335e53559e707abe18f5cccd6e97a255a5bda7c
Mentor Palokaj <mentor@palokaj.co>
2020-04-23 11:40:15 +0200

    Config CI to work with multiple prohects

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,36 @@
 const functions = require('firebase-functions')
 
 const admin = require('firebase-admin')
-admin.initializeApp()
-const db = admin.firestore()
+const app = admin.initializeApp()
+const { FieldValue } = app.firestore
+const db = app.firestore()
 
 // ///////////////////////////////
 // On relation create IN PROGRESS
 // ///////////////////////////////
-// exports.modifyUser = functions.firestore.document( 'relationships/{relationId}' ).onWrite( ( change, context ) => {
+exports.unFollow = functions.firestore.document( 'relationships/{relationId}' ).onDelete( ( snap, context ) => {
 
-// 	const before = change.before.exists ? { id: change.before.id, ...change.before.data() } : null
-// 	const { follower, author } = before
+	const { author, follower } = snap.data()
+	return Promise.all( [
+		// Remove from author's followers
+		db.collection( 'userMeta' ).doc( author ).set( { followers: FieldValue.arrayRemove( follower ) }, { merge: true } ),
+		// Remove from follower's following
+		db.collection( 'userMeta' ).doc( follower ).set( { following: FieldValue.arrayRemove( author ) }, { merge: true } )
+	] )
 
-// 	const after  = change.after.exists ? { id: change.after.id, ...change.after.data() } : null
+} )
 
-// 	// If change was a deletion, delete the relevant references
-// 	if( !after ) {
-// 		return Promise.all( [
-// 			db.collection( 'userMeta' ).doc( author ),
-// 			db.collection( 'userMeta' ).doc( doc.id ).delete()
-// 		] )
-// 	}
+exports.follow = functions.firestore.document( 'relationships/{relationId}' ).onWrite( ( change, context ) => {
 
+	// Ignore deletiona
+	if( !change.after.exists ) return
+		
+	const { author, follower } = change.after.data()
+	return Promise.all( [
+		// Remove from author's followers
+		db.collection( 'userMeta' ).doc( author ).set( { followers: FieldValue.arrayUnion( follower ) }, { merge: true } ),
+		// Remove from follower's following
+		db.collection( 'userMeta' ).doc( follower ).set( { following: FieldValue.arrayUnion( author ) }, { merge: true } )
+	] )
 
-
-// } )
+} )
