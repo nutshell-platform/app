72feee4e0594bc5800144da79ce45324824b8a1f
Mentor Palokaj <mentor@palokaj.co>
2020-06-19 20:50:05 +0200

    Console process

--- src/modules/apis/platform.js
+++ src/modules/apis/platform.js
@@ -8,6 +8,8 @@ export const isAndroid = Platform.OS == 'android'
 export const dev = process.env.NODE_ENV == 'development' || ( isWeb && typeof location !== 'undefined' && location.href.includes( 'debug=true' ) )
 export const isCI = !!process.env.CI
 
+console.log( process.env, isCI )
+
 const { version: appVersion, revisionId } = manifest
 export const version = revisionId || 'development'
 
