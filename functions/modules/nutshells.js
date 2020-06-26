9fc9fced293185d311b51ea0d5479a6798a016d5
Mentor Palokaj <mentor@palokaj.co>
2020-06-26 10:40:08 +0200

    Use awaits instead of returns which works un offline emulator

--- functions/modules/nutshells.js
+++ functions/modules/nutshells.js
@@ -3,6 +3,38 @@ const { dataFromSnap, log, error } = require( './helpers' )
 const { db, FieldValue } = require( './firebase' )
 
 // ///////////////////////////////
+// Demo data
+// ///////////////////////////////
+exports.makeDemo = async f => {
+
+	const nutshells = [ 1,2,3,4,5 ]
+
+	try {
+		await Promise.all( nutshells.map( nutshell => {
+
+			return db.collection( 'nutshells' ).add( {
+				created: Date.now(),
+				owner: 'bHOwM0nKgPNMet0JGMCczdbfIrz2', // Derpface
+				published: Date.now(),
+				status: 'scheduled',
+				updated: Date.now(),
+				entries: [
+					{ uuid: Math.random(), title: 'Demo', paragraph: 'Hello' }
+				]
+			} )
+
+		} ) )
+
+		console.log( 'Creation success' )
+
+	} catch( e ) {
+		console.log( 'creation problem, ', e )
+	}
+
+}
+
+
+// ///////////////////////////////
 // Publish scheduled Nutshells
 // ///////////////////////////////
 exports.publish = async f => {
@@ -28,33 +60,44 @@ exports.publish = async f => {
 		// For every Nutshell send to inboxed
 		// ///////////////////////////////
 		runLog.push( 'Sending nutshells to inbox' )
-		return Promise.all( nutshells.map( async nutshell => {
 
-				runLog.push( `Parsing ${ nutshell.uid }` )
+		await Promise.all( nutshells.map( async nutshell => {
 
-				// Get the followers of the owner of this Nutshell
-				const { followers } = await db.collection( 'userMeta' ).doc( nutshell.owner ).get().then( dataFromSnap ).catch( f => ( { followers: [] } ) )
+				try {
 
-				runLog.push( `Nutshell ${ nutshell.uid } has ${ followers && followers.length } followers` )
+					runLog.push( `Parsing ${ nutshell.uid }` )
 
-				// For every follower, add this Nutshell to their inbox
-				if( followers && followers.length != 0 ) await Promise.all( followers.map( followerUid => {
-					return db.collection( 'inbox' ).doc( followerUid ).set( { nutshells: FieldValue.arrayUnion( nutshell.uid ) }, { merge: true } )
+					// Get the followers of the owner of this Nutshell
+					const { followers } = await db.collection( 'userMeta' ).doc( nutshell.owner ).get().then( dataFromSnap )
+
+					runLog.push( `Nutshell ${ nutshell.uid } has ${ followers && followers.length } followers` )
+
+					// For every follower, add this Nutshell to their inbox
+					if( followers && followers.length != 0 ) await Promise.all( followers.map( followerUid => {
+						return db.collection( 'inbox' ).doc( followerUid ).set( { nutshells: FieldValue.arrayUnion( nutshell.uid ) }, { merge: true } )
+							.catch( e => {
+								runLog.push( `Error adding ${ nutshell.uid } to inbox of ${ followerUid }` )
+								runLog.push( e )
+								throw e
+							} )
+					} ) )
+
+					// Once added to inboxes, mark published
+					runLog.push(  `Marking nutshell ${ nutshell.uid } as read` )
+					return db.collection( 'nutshells' ).doc( nutshell.uid ).set( { status: 'published' }, { merge: true } )
 						.catch( e => {
-							runLog.push( `Error adding ${ nutshell.uid } to inbox of ${ followerUid }` )
+							runLog.push( `Error marking ${ nutshell.uid } as read` )
 							runLog.push( e )
 							throw e
 						} )
-				} ) )
 
-				// Once added to inboxes, mark published
-				runLog.push(  `Marking nutshell ${ nutshell.uid } as read` )
-				return db.collection( 'nutshells' ).doc( nutshell.uid ).set( { status: 'published' }, { merge: true } )
-					.catch( e => {
-						runLog.push( `Error marking ${ nutshell.uid } as read` )
-						runLog.push( e )
-						throw e
-					} )
+				} catch( e ) {
+
+					log.push( 'Problem parsing nutshell:' )
+					log.push( e )
+					throw e
+
+				}
 
 		} ) )
 
@@ -82,7 +125,7 @@ exports.deleteFromUnboxesOnNutshellDelete = async ( snap, context ) => {
 		if( !followerRelations || followerRelations.length == 0 ) return null
 
 		// For each loader update the inbox to no longer have this Nutshell
-		return Promise.all( followerRelations.map( ( { follower } ) => {
+		await Promise.all( followerRelations.map( ( { follower } ) => {
 			return db.collection( 'inbox' ).doc( follower ).set( { nutshells: FieldValue.arrayRemove( nutshellUid ) }, { merge: true } ).catch( e => error( `Error marking nutshell ${ nutshellUid } read for ${ follower }: `, e ) )
 		} ) )
 
