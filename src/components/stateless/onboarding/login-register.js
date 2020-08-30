9015961cf8d8e98655c5d1d35df36794f107bf31
Mentor Palokaj <mentor@palokaj.co>
2020-08-30 11:10:47 +0200

    Onboarding no capitalisation and proper autofills

--- src/components/stateless/onboarding/login-register.js
+++ src/components/stateless/onboarding/login-register.js
@@ -3,11 +3,12 @@ import { View } from 'react-native'
 import { Card, Input, Button, Divider, Text, Checkbox, Link } from '../common/generic'
 
 export const Login = ( { toggle, proceed, onInput, action='login', name, handle, email, password, available, tos } ) => <Card>
+
 		{ /* User input */ }
-		{ action == 'register' && <Input onSubmit={ proceed } value={ name } onChangeText={ t => onInput( 'name', t ) } label='name' /> }
-		{ action == 'register' && <Input onSubmit={ proceed } error={ !available && 'This handle is taken' } info={ 'Your handle is a unique username you can share with your friends so they can easily find you.' } value={ handle } onChangeText={ t => onInput( 'handle', t ) } label='username/handle' /> }
-		<Input onSubmit={ proceed } value={ email } onChangeText={ t => onInput( 'email', t ) } label='email' keyboardType='email-address'/>
-		{ action != 'recover' && <Input onSubmit={ proceed } value={ password } onChangeText={ t => onInput( 'password', t ) } secureTextEntry={ true } label='password' /> }
+		{ action == 'register' && <Input autoCompleteType='name' onSubmit={ proceed } value={ name } onChangeText={ t => onInput( 'name', t ) } label='name' /> }
+		{ action == 'register' && <Input autoCompleteType='username' autoCapitalize='none' onSubmit={ proceed } error={ !available && 'This handle is taken' } info={ 'Your handle is a unique username you can share with your friends so they can easily find you.' } value={ handle } onChangeText={ t => onInput( 'handle', t ) } label='username/handle' /> }
+		<Input autoCompleteType='password' autoCapitalize='none' onSubmit={ proceed } value={ email } onChangeText={ t => onInput( 'email', t ) } label='email' keyboardType='email-address'/>
+		{ action != 'recover' && <Input autoCapitalize='none' onSubmit={ proceed } value={ password } onChangeText={ t => onInput( 'password', t ) } secureTextEntry={ true } label='password' /> }
 
 		{ action == 'register' && <Checkbox onPress={ f => onInput( 'tos', !tos ) } checked={ tos } style={ { marginTop: 10 } }>
 			<Text onPress={ f => onInput( 'tos', !tos ) }>I accept the </Text>
