bce8fea8685e8cfafcc898d8733691813a860a70
Mentor Palokaj <mentor@palokaj.co>
2020-08-30 16:13:19 +0200

    Updates tests

--- src/components/stateful/nutshells/read.js
+++ src/components/stateful/nutshells/read.js
@@ -134,7 +134,7 @@ class ReadNutshell extends Component {
 	render() {
 
 		const { loading, inbox, rawInbox } = this.state
-		const { user, history } = this.props
+		const { user, history, draft } = this.props
 
 		return <Container Background={ People }>
 			<Navigation title='Home' />
