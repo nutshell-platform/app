9c75e5301d28c1118a1ddb929828f5cc02611bf2
Mentor Palokaj <mentor@palokaj.co>
2020-10-27 18:33:04 +0100

    increment binary counter

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
 const conf = {
   dev: process.env.development,
   bundle: 'com.nutshell.nutshell', // ⚠️ Reverse DNS, same as android
-  version: 9, // ⚠️ Update on build
+  version: 10, // ⚠️ Update on build
 }
 
 export default {
