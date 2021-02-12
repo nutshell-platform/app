b1d39d7b8929b59cd0072521303d3ecf9410e8e1
Mentor Palokaj <mentor@palokaj.co>
2021-02-12 14:33:36 +0100

    update test snaps

--- app.config.js
+++ app.config.js
@@ -73,7 +73,8 @@ module.exports = {
       "infoPlist": {
         NSCameraUsageDescription: "Camera permission is used to take a new photo to use as your profile picture.",
         NSPhotoLibraryUsageDescription: "Photo library permission is used to select an existing photo on your device as a profile picture.",
-        NSContactsUsageDescription: "Contact book permission is used to find your friends on the platform, you will need to trigger this action manually. Your contacts are never accessed without your consent."
+        NSContactsUsageDescription: "Contact book permission is used to find your friends on the platform, you will need to trigger this action manually. Your contacts are never accessed without your consent.",
+        NSMicrophoneUsageDescription: "Recording permission is used to allow you to record voice updates in your Nutshell. This permission is only used when you are explicitly recording and never in the background."
       }
     },
 
@@ -82,7 +83,7 @@ module.exports = {
       "package": "com.nutshell.nutshell",
       "googleServicesFile": `./google-services${ conf.dev ? '-development' : '' }.json`,
       "versionCode": conf.version,
-      "permissions": [ "CAMERA", "READ_EXTERNAL_STORAGE", "WRITE_EXTERNAL_STORAGE", "READ_CONTACTS" ],
+      "permissions": [ "CAMERA", "READ_EXTERNAL_STORAGE", "WRITE_EXTERNAL_STORAGE", "READ_CONTACTS", "RECORD_AUDIO" ],
       icon: "./assets/icon/app/app-icon-solid-bg-rounded.png",
       adaptiveIcon: {
         foregroundImage: "./assets/icon/app/app-icon-alpha-adaptive-108-outer-72-inner.png",
