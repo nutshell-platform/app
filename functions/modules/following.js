6b1b3cd12199f44d1d0d12ad77be537b7ffa446c
Mentor Palokaj <mentor@palokaj.co>
2021-05-13 09:42:09 +0200

    Add follow push noti to following action

--- functions/modules/following.js
+++ functions/modules/following.js
@@ -1,4 +1,6 @@
 const { db, FieldValue } = require( './firebase' )
+const { dataFromSnap, log, error } = require( './helpers' )
+const { sendPushNotifications } = require( './push' )
 
 // ( snap, context ) =>
 exports.unfollow = snap => {
@@ -12,15 +14,41 @@ exports.unfollow = snap => {
 }
 
 // ( change, context ) =>
-exports.follow = change => {
-	// Ignore deletiona
-	if( !change.after.exists ) return null
-		
-	const { author, follower } = change.after.data()
-	return Promise.all( [
-		// Remove from author's followers
-		db.collection( 'userMeta' ).doc( author ).set( { followers: FieldValue.arrayUnion( follower ) }, { merge: true } ),
-		// Remove from follower's following
-		db.collection( 'userMeta' ).doc( follower ).set( { following: FieldValue.arrayUnion( author ) }, { merge: true } )
-	] )
+exports.follow = async change => {
+	
+	try {
+
+		// Ignore deletiona
+		if( !change.after.exists ) return null
+			
+		const { author, follower } = change.after.data()
+
+		// Set following statusses
+		await Promise.all( [
+			// Remove from author's followers
+			db.collection( 'userMeta' ).doc( author ).set( { followers: FieldValue.arrayUnion( follower ) }, { merge: true } ),
+			// Remove from follower's following
+			db.collection( 'userMeta' ).doc( follower ).set( { following: FieldValue.arrayUnion( author ) }, { merge: true } )
+		] )
+
+		// Check if author has push tokens, exit if not
+		const { pushTokens=[] } = await db.collection( 'settings' ).doc( author ).get().then( dataFromSnap ) || {}
+		if( !pushTokens.length ) return
+
+		// Get the data of the new follower
+		const { name, handle } = await db.collection( 'users' ).doc( follower ).get().then( dataFromSnap ) || {}
+
+		// Send push notification to author
+		await sendPushNotifications( pushTokens, {
+			title: `${ name || handle || 'Someone' } followed you!`,
+			body: `Tap to view ${ handle ? `${ handle || name }'s` : 'their' } profile.`,
+			data: {
+				goto: handle ? `/${ handle }` : '/friends/find'
+			}
+		} )
+
+	} catch( e ) {
+		error( 'Error in follow: ', e )
+	}
+
 }
